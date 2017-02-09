8a0345112abd60c1febcfd6547a1a51b0c9ee229
Pi <adlhancock@gmail.com>
2017-02-09 16:29:01 +0000

    still tweaking

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ class Mock(MagicMock):
         def __getattr__(cls, name):
                 return MagicMock()
 
-MOCK_MODULES = ['RPi.GPIO']
+MOCK_MODULES = ['RPi.GPIO', 'RPi']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
