4c353e6c824980e9708866618105d756ce0bca69
Pi <adlhancock@gmail.com>
2017-02-09 16:25:09 +0000

    still tweaking

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,20 @@ sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 
 
+# this should solve the ImportErrors for RPi
+#
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+        @classmethod
+        def __getattr__(cls, name):
+                return MagicMock()
+
+MOCK_MODULES = ['RPi']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_modules)
+
+
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
